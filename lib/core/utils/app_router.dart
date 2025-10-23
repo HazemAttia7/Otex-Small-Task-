3cdf7092895576b6aa37fae50cfe33c8cf89aa3c
HazemAttia7 <lolhazem41@gmail.com>
2025-10-23 19:17:18 +0300

    chore: moved the MultiBlocProvider from main -> GoRoute

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,35 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:otex_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:otex_app/features/home/presentation/manager/categories_cubit/categories_cubit.dart';
+import 'package:otex_app/features/home/presentation/manager/products_cubit/products_cubit.dart';
+import 'package:otex_app/features/home/presentation/manager/sub_categories_cubit/sub_categories_cubit.dart';
 import 'package:otex_app/features/home/presentation/views/home_view.dart';
 
 abstract class AppRouter {
   static const kHome = "/";
-  static GoRouter router = GoRouter(routes: [
-      GoRoute(path: kHome, builder: (context, state) => const HomeView()),
-  ]);
+  static GoRouter router = GoRouter(
+    routes: [
+      GoRoute(
+        path: kHome,
+        builder: (context, state) => MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) =>
+                  CategoriesCubit(HomeRepoImpl())..fetchAllCategories(),
+            ),
+            BlocProvider(
+              create: (context) =>
+                  SubCategoriesCubit(HomeRepoImpl())..fetchAllSubcategories(),
+            ),
+            BlocProvider(
+              create: (context) =>
+                  ProductsCubit(HomeRepoImpl())..fetchAllProducts(),
+            ),
+          ],
+          child: const HomeView(),
+        ),
+      ),
+    ],
+  );
 }
