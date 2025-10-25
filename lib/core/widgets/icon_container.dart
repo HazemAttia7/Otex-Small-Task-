968ca586f9269ef216cae25e46521ed4c1654c99
HazemAttia7 <lolhazem41@gmail.com>
2025-10-25 18:35:11 +0300

    chore: updated company badge

--- lib/core/widgets/icon_container.dart
+++ lib/core/widgets/icon_container.dart
@@ -4,20 +4,21 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class IconContainer extends StatelessWidget {
   final IconData icon;
   final Color backColor, iconColor;
-  final double? iconSize, padding;
+  final double? iconSize;
+  final EdgeInsetsGeometry? padding;
   const IconContainer({
     super.key,
     required this.icon,
     required this.backColor,
     required this.iconColor,
-    this.iconSize,
     this.padding,
+    this.iconSize,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(padding ?? 10.sp),
+      padding: padding ?? EdgeInsets.all(10.sp),
       decoration: BoxDecoration(shape: BoxShape.circle, color: backColor),
       child: Icon(icon, color: iconColor, size: iconSize ?? 18.sp),
     );
